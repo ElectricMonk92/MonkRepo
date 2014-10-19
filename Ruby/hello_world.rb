ab28cc81225d28ff4f7b5b4b869e0b981c82f99b
ElectricMonk92 <truebrit.jackson@gmail.com>
2014-10-19 14:44:08 +0100

    Added Bath_Events Rails Prject with events modela and user model.

--- Ruby/hello_world.rb
+++ Ruby/hello_world.rb
@@ -1,1 +1,17 @@
 puts "Hello World!!!"
+
+def print_times ( str, num )
+	num.times do puts str end
+end
+
+print_times "Hello World", 10
+
+10.times do
+	print_times "Yo!", 10
+end 
+
+new_array = (0..20).inject(0) {|sum, var|
+	var + sum
+ }
+
+p new_array
